32e352c29bd26a4509cab3870a94b3edc5606d45
TiA4f8R <74829229+TiA4f8R@users.noreply.github.com>
2021-06-18 21:36:08 +0200

    Add partial support of other delivery methods in the player only

--- app/src/main/java/org/schabi/newpipe/player/resolver/PlaybackResolver.java
+++ app/src/main/java/org/schabi/newpipe/player/resolver/PlaybackResolver.java
@@ -1,7 +1,6 @@
 package org.schabi.newpipe.player.resolver;
 
 import android.net.Uri;
-import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,11 +8,18 @@ import androidx.annotation.Nullable;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.source.dash.manifest.DashManifest;
+import com.google.android.exoplayer2.source.dash.manifest.DashManifestParser;
 
+import org.schabi.newpipe.extractor.stream.DeliveryMethod;
+import org.schabi.newpipe.extractor.stream.Stream;
 import org.schabi.newpipe.extractor.stream.StreamInfo;
 import org.schabi.newpipe.extractor.stream.StreamType;
 import org.schabi.newpipe.player.helper.PlayerDataSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public interface PlaybackResolver extends Resolver<StreamInfo, MediaSource> {
 
@@ -58,29 +64,44 @@ public interface PlaybackResolver extends Resolver<StreamInfo, MediaSource> {
 
     @NonNull
     default MediaSource buildMediaSource(@NonNull final PlayerDataSource dataSource,
-                                         @NonNull final String sourceUrl,
+                                         @NonNull final Stream stream,
                                          @NonNull final String cacheKey,
-                                         @NonNull final String overrideExtension,
-                                         @NonNull final MediaSourceTag metadata) {
-        final Uri uri = Uri.parse(sourceUrl);
-        @C.ContentType final int type = TextUtils.isEmpty(overrideExtension)
-                ? Util.inferContentType(uri) : Util.inferContentType("." + overrideExtension);
-
-        switch (type) {
-            case C.TYPE_SS:
-                return dataSource.getLiveSsMediaSourceFactory().setTag(metadata)
-                        .createMediaSource(MediaItem.fromUri(uri));
-            case C.TYPE_DASH:
-                return dataSource.getDashMediaSourceFactory().setTag(metadata)
-                        .createMediaSource(MediaItem.fromUri(uri));
-            case C.TYPE_HLS:
+                                         @NonNull final MediaSourceTag metadata)
+            throws IOException {
+        final DeliveryMethod deliveryFormat = stream.getDeliveryMethod();
+        if (deliveryFormat.equals(DeliveryMethod.PROGRESSIVE_HTTP)
+                || deliveryFormat.equals(DeliveryMethod.TORRENT)) {
+            final String url = stream.getContent();
+            return dataSource.getExtractorMediaSourceFactory(cacheKey).setTag(metadata)
+                    .createMediaSource(MediaItem.fromUri((url)));
+        } else if (deliveryFormat.equals(DeliveryMethod.HLS)) {
+            if (stream.isUrl()) {
                 return dataSource.getHlsMediaSourceFactory().setTag(metadata)
-                        .createMediaSource(MediaItem.fromUri(uri));
-            case C.TYPE_OTHER:
-                return dataSource.getExtractorMediaSourceFactory(cacheKey).setTag(metadata)
-                        .createMediaSource(MediaItem.fromUri(uri));
-            default:
-                throw new IllegalStateException("Unsupported type: " + type);
+                        .createMediaSource(MediaItem.fromUri(Uri.parse(stream.getContent())));
+            } else {
+                throw new IllegalArgumentException(
+                        "HLS streams which are not URLs are not supported");
+            }
+            // Other contents are not supported right now
+        } else if (deliveryFormat.equals(DeliveryMethod.DASH)) {
+            if (stream.isUrl()) {
+                return dataSource.getDashMediaSourceFactory().setTag(metadata)
+                        .createMediaSource(MediaItem.fromUri(Uri.parse(stream.getContent())));
+            } else {
+                final DashManifest dashManifest;
+                try {
+                    final ByteArrayInputStream dashManifestInput = new ByteArrayInputStream(
+                            stream.getContent().getBytes(StandardCharsets.UTF_8));
+                    dashManifest = new DashManifestParser().parse(Uri.parse(stream.getBaseUrl()),
+                            dashManifestInput);
+                } catch (final IOException e) {
+                    throw new IOException("Error when parsing manual DASH manifest", e);
+                }
+                return dataSource.getDashMediaSourceFactory().setTag(metadata)
+                        .createMediaSource(dashManifest);
+            }
+        } else {
+            throw new IllegalArgumentException("Unsupported delivery type" + deliveryFormat);
         }
     }
 }
